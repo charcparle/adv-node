ae6486043b0a0e7501cec1136568900322a7b5a3
Charles C <charles.yhcheng@gmail.com>
2020-12-07 15:08:21 +0000

    initialized passport and session

--- server.js
+++ server.js
@@ -3,18 +3,43 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
+// Request Logger
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug')
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});;
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port ' + process.env.PORT);
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
 });
+
+// Generate hash for session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+// Passport initialize
+app.use(passport.initialize());
+app.use(passport.session());
+
+
